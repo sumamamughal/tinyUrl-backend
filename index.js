73878e3a32ce150bafae23d753090841b76fdab1
sumamamughal <sumamaahmad777@gmail.com>
2026-04-06 14:26:51 +0500

    add vercel.json file

--- index.js
+++ index.js
@@ -44,8 +44,11 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server ready at http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`🚀 Server ready at http://localhost:${PORT}`);
+  });
+}
 
+export default app;
